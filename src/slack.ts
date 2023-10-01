c2355e5fcdf7fec794ec8843044cdf3654c43d9b
Matilda Mared <matildamared@live.se>
2023-10-01 11:21:45 +0200

    add handler

--- src/slack.ts
+++ src/slack.ts
@@ -1,14 +1,46 @@
-import type { Handler } from '@netlify/functions';
+import type { Handler } from "@netlify/functions";
+import { parse } from "querystring";
+import { slackApi } from "./util/slack";
 
-export const handler: Handler = async () => {
+async function handleSlashCommand(payload: SlackSlashCommandPayload) {
+	switch (payload.command) {
+		case "/foodfight":
+			const response = await slackApi("chat.postMessage", {
+				channel: payload.channel_id,
+				text: "Hello, world!",
+			});
+
+			if (!response.ok) {
+				console.error(response);
+				throw new Error("Failed to post message to Slack");
+			}
+			break;
+
+		default:
+			return {
+				statusCode: 200,
+				body: `Unknown command: ${payload.command}`,
+			};
+	}
+
+	return {
+		statusCode: 200,
+		body: "",
+	};
+}
+
+export const handler: Handler = async (event) => {
 	// TODO validate the Slack request
 
-	// TODO handle slash commands
+	const body = parse(event.body ?? "") as SlackPayload;
+	if (body.command) {
+		return handleSlashCommand(body as SlackSlashCommandPayload);
+	}
 
 	// TODO handle interactivity (e.g. context commands, modals)
 
 	return {
 		statusCode: 200,
-		body: 'TODO: handle Slack commands and interactivity!!',
+		body: "TODO: handle Slack commands and interactivity!!",
 	};
 };
