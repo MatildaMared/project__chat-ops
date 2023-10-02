6a576639e2f2e1dda8d9dfc2b6cfa93f62c0ce04
Matilda Mared <matildamared@live.se>
2023-10-02 06:51:59 +0200

    update handler to create modal

--- src/slack.ts
+++ src/slack.ts
@@ -1,20 +1,41 @@
 import type { Handler } from "@netlify/functions";
 import { parse } from "querystring";
-import { slackApi, verifySlackRequest } from "./util/slack";
+import { slackApi, verifySlackRequest, blocks, modal } from "./util/slack";
 
 async function handleSlashCommand(payload: SlackSlashCommandPayload) {
 	switch (payload.command) {
 		case "/foodfight":
-			const joke = await fetch("https://icanhazdadjoke.com/", {
-				headers: {
-					Accept: "text/plain",
-				},
-			});
-
-			const response = await slackApi("chat.postMessage", {
-				channel: payload.channel_id,
-				text: await joke.text(),
-			});
+			const response = await slackApi(
+				"views.open",
+				modal({
+					id: "foodfight-modal",
+					title: "Start a food fight",
+					trigger_id: payload.trigger_id,
+					blocks: [
+						blocks.section({
+							text: "The discourse demands food drama! *Send in your spiciest food takes so we can all argue about them and feel alive.*",
+						}),
+						blocks.input({
+							id: "opinion",
+							label: "Deposit your controversial food opinion here",
+							placeholder: "I think pineapple belongs on pizza",
+							initial_value: payload.text ?? "",
+							hint: "What do you believe about food that people find apalling? Say it with your chest!",
+						}),
+						blocks.select({
+							id: "spice_level",
+							label: "How spicy is this opinion?",
+							placeholder: "Select a spice level",
+							options: [
+								{ label: "mild", value: "mild" },
+								{ label: "medium", value: "medium" },
+								{ label: "spicy", value: "spicy" },
+								{ label: "nuclear", value: "nuclear" },
+							],
+						}),
+					],
+				})
+			);
 
 			if (!response.ok) {
 				console.error(response);
